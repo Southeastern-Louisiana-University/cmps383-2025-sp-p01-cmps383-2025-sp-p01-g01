e55ec51a44fc8accdda1b3a7cb48b1d12517ad57
brad jarreau <brad.jarreau@selu.edu>
2025-02-04 13:34:01 -0600

    Swagger
    updated swagger to get it up and running

--- Selu383.SP25.Api/Program.cs
+++ Selu383.SP25.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 
 namespace Selu383.SP25.Api
 {
@@ -8,26 +9,34 @@ namespace Selu383.SP25.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            // Add Swagger services
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Selu API",
+                    Version = "v1"
+                });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Selu API v1");
+                });
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
